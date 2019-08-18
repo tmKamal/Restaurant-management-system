9d5fd5540ec13b985b10caf2a04e171a1fb11424
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2019-08-18 20:53:13 +0530

    Login function completed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,49 +1,53 @@
 @extends('layouts.loginform')
 @section('content')
-    <!--form-->
-    <form method="POST"> 
-                <div class="login-title mb-3">
-                    Login
-                </div>
-                <!-- @csrf -->
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text" id="inputGroupPrepend"><i class="material-icons">account_circle</i></span>
-                    </div>
-                    <input type="text" class="form-control" id="validationCustomUsername" placeholder="Email"
-                        aria-describedby="inputGroupPrepend" required>
-                    <div class="invalid-feedback">
-                        Email
-                    </div>
-                </div>
-                <div class="input-group mb-3 mt-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="inputGroupPrepend"><i class="material-icons">lock</i>
-                        </div>
-                        <input type="text" class="form-control" id="validationCustomUsername" placeholder="Password"
-                            aria-describedby="inputGroupPrepend" required>
-                        <div class="invalid-feedback">
-                            Password
-                        </div>
-                    </div>
-                    <div class="custom-control custom-checkbox my-1 mr-sm-2">
-                            <input type="checkbox" class="custom-control-input" id="customControlInline">
-                            <label class="custom-control-label" for="customControlInline">Remember Me</label>
-                    </div>
+<!--form-->
+<form id="loginForm" method="POST">
+    <div class="login-title mb-3">
+        Login
+    </div>
+     @csrf
+    <div class="input-group">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroupPrepend"><i class="material-icons">account_circle</i></span>
+        </div>
+        <input id="email" name="email" type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{old('email')}}" aria-describedby="inputGroupPrepend" required autocomplete="email" autofocus>
+        @error('email')
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror
+    </div>
+    <div class="input-group mb-3 mt-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroupPrepend"><i class="material-icons">lock</i>
+        </div>
+        <input id="password" name="password" type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" aria-describedby="inputGroupPrepend" required autocomplete="current-password">
+        @error('password')
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror
+    </div>
+    <div class="custom-control custom-checkbox my-1 mr-sm-2">
+        <input name="remember" id="remember" type="checkbox" class="custom-control-input" {{old('remember')?'checked':''}}>
+        <label class="custom-control-label" for="customControlInline">Remember Me</label>
+    </div>
+    @if(Route::has('password.request'))
+    <div class="form-group">
+        <a href="#" onclick="document.getElementById('loginForm').submit()" class="loginButton mb-1">LOGIN</a>
+        <div class="forgot-pass ">
+        <h6>Forgot <a href="{{route('password.request')}}">Password?</a></h6>
+        </div>
+    </div>
+    @endif
 
-                <div class="form-group">
-                         <a href="#" class="loginButton mb-1">LOGIN</a>
-                         <div class="forgot-pass ">
-                            <h6>Forgot <a href="#">Password?</a></h6>
-                         </div>
-                </div>
-                <hr>
-                <div class="form-group">
-                        
-                        <div class="forgot-pass mt-4">
-                           <h6>Don't have an account? <a href="#" class="ml-2">Sign Up Here!</a></h6>
-                        </div>
-               </div>
-            </form>
+    <hr>
+    <div class="form-group">
+
+        <div class="forgot-pass mt-4">
+        <h6>Don't have an account? <a href="{{route('register')}}" class="ml-2">Sign Up Here!</a></h6>
+        </div>
+    </div>
+</form>
 
 @endSection
