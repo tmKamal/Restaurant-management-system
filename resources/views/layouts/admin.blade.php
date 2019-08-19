8caf17235072b9fe3fe938a086257ae640ac6e7c
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2019-08-19 10:17:24 +0530

    logout function

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -87,7 +87,12 @@
                                                 <i class="material-icons crsidebarIcon">exit_to_app</i>
                                             </div>
                                             <div class="col-10 sidebarText">
-                                                LOG OUT
+                                                <a href="{{ route('logout') }}"
+                                                onclick="event.preventDefault();
+                                                              document.getElementById('logout-form').submit();">LOG OUT</a> 
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                    @csrf
+                                                </form>
                                             </div>
                                         </div>
                                     </li>
